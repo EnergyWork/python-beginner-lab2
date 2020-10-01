0dbf37b0b3894876516bd5aed3785fb902c8d9ec
Artyom <ag.energy@mail.ru>
2020-10-01 20:42:38 +0300

    Update task7.py

--- task7.py
+++ task7.py
@@ -1,12 +1,28 @@
 import re
 
-s = 'Тире - слово! Разве? Собака, кошка, попугай. Предложение; точка с запятой? \
-    Обобщающее слово: слово1, слово2, слово3(привет), - дальше... [1], выборка{aqadaf}'
+s = 'Тире-слово !Разве ? Собака , кошка ,попугай . Предложение ; точка с запятой ? \
+    Обобщающее слово : слово1 , слово2 , слово3 ( привет ) , -дальше.. . [1 ] , выборка{ aqadaf }'
 
 # simple
 str1 = s
-
-
+print(str1)
+str1 = str1.replace('.','. ')
+str1 = str1.replace('?','? ')
+str1 = str1.replace('!','! ')
+str1 = str1.replace(',',', ')
+str1 = str1.replace(':',': ')
+str1 = str1.replace(',',', ')
+str1 = str1.replace(')',') ')
+str1 = str1.replace('}','} ')
+str1 = str1.replace(']','] ')
+str1 = str1.replace('(',' (')
+str1 = str1.replace('[',' [')
+str1 = str1.replace('{',' {')
+str1 = str1.replace('.  .','..')
+str1 = str1.replace('-',' - ')
+str1 = str1.replace('. .','..')
+str1 = str1.replace('  ',' ')
+print()
 print(str1)
 
 #regex
